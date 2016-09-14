353e2fedea897823bdd6e824a2b96e0ea8963187
Ryan Van Rooyen <ryan.vanrooyen@icloud.com>
2016-09-14 12:23:01 -0600

    Added some initial project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
