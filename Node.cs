e3a76d645b97e2f6ab1231e39b68e77d26be14ec
Ryan Van Rooyen <ryan.vanrooyen@icloud.com>
2016-09-20 12:17:58 -0600

    Trying out mono versions.

--- Node.cs
+++ Node.cs
@@ -3,51 +3,51 @@ using System;
 
 namespace BehaviorTree
 {
-    public enum Result
-    {
-        Success,
-        Running,
-        Failure
-    }
+	public enum Result
+	{
+		Success,
+		Running,
+		Failure
+	}
 
-    public interface INode
-    {
-        Result Run();
-    }
+	public interface INode
+	{
+		Result Run();
+	}
 
-    public class Node : INode
-    {
-        private readonly Func<Result> behavior;
+	public class Node : INode
+	{
+		private readonly Func<Result> behavior;
 
-        public Node(Func<Result> behavior)
-        {
-            if (behavior == null)
-                throw new ArgumentNullException("behavior");
+		public Node(Func<Result> behavior)
+		{
+			if (behavior == null)
+				throw new ArgumentNullException(nameof(behavior));
 
-            this.behavior = behavior;
-        }
+			this.behavior = behavior;
+		}
 
-        public Result Run()
-        {
-            return this.behavior();
-        }
-    }
+		public Result Run()
+		{
+			return this.behavior();
+		}
+	}
 
-    public class Succeed : Node
-    {
-        public Succeed() : base(() => Result.Success)
-        {}
-    }
+	public class Succeed : Node
+	{
+		public Succeed() : base(() => Result.Success)
+		{ }
+	}
 
-    public class Fail : Node
-    {
-        public Fail() : base(() => Result.Failure)
-        {}
-    }
+	public class Fail : Node
+	{
+		public Fail() : base(() => Result.Failure)
+		{ }
+	}
 
-    public class Running : Node
-    {
-        public Running() : base(() => Result.Running)
-        {}
-    }
+	public class Running : Node
+	{
+		public Running() : base(() => Result.Running)
+		{ }
+	}
 }
