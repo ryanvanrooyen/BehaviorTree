28d0949ed866f1a6b0dd426974268408db9671d8
Ryan Van Rooyen <ryan.vanrooyen@icloud.com>
2016-10-20 23:03:34 -0600

    Added an If and Action node type.

--- Node.cs
+++ Node.cs
@@ -16,6 +16,7 @@ namespace BehaviorTree
 	}
 
 	public delegate T Func<T>();
+	public delegate void Func();
 
 	public class Node : INode
 	{
@@ -42,5 +43,46 @@ namespace BehaviorTree
 
 		public static readonly INode Running
 			= new Node(() => Result.Running);
+	}
+
+	public class If : INode
+	{
+		private readonly Func<bool> condition;
+
+		public If(Func<bool> condition)
+		{
+			if (condition == null)
+				throw new ArgumentNullException(nameof(condition));
+
+			this.condition = condition;
+		}
+
+		public Result Run()
+		{
+			var success = this.condition();
+			if (success)
+				return Result.Success;
+
+			return Result.Failure;
+		}
+	}
+
+	public class Action : INode
+	{
+		private readonly Func action;
+
+		public Action(Func action)
+		{
+			if (action == null)
+				throw new ArgumentNullException(nameof(action));
+
+			this.action = action;
+		}
+
+		public Result Run()
+		{
+			this.action();
+			return Result.Success;
+		}
 	}
 }
