31cbc7005a2fbd72b086a0b8d81ab63f8de76c2d
Ryan Van Rooyen <ryan.vanrooyen@icloud.com>
2016-11-29 14:32:00 -0700

    Fixed issues with While node.

--- Composites/While.cs
+++ Composites/While.cs
@@ -6,38 +6,101 @@ namespace BehaviorTree
 	public class While : Composite
 	{
 		public While(Delegates.Func<bool> condition, INode trueAction)
-			: base("While", Result.Success, new If(condition),
-				new ParallelSequence("Parallel", new If(condition), trueAction))
-		{
-		}
+			: base("While", Result.Success, CreateIfNodes(condition, trueAction))
+		{ }
 
 		public While(Delegates.Func<bool> condition, INode trueAction, INode falseAction)
-			: base("While", Result.Failure,
-				   CreateTrueBranch(condition, trueAction),
-				   CreateFalseBranch(condition, falseAction))
-		{
-		}
+			: base("While", Result.Failure, CreateIfElseNodes(condition, trueAction, falseAction))
+		{ }
 
-		private static INode CreateTrueBranch(Delegates.Func<bool> condition, INode trueAction)
+		private static INode[] CreateIfNodes(Delegates.Func<bool> condition, INode trueAction)
 		{
 			if (condition == null)
 				throw new ArgumentNullException("condition");
 			if (trueAction == null)
 				throw new ArgumentNullException("trueAction");
+			
+			var whileIf = new WhileIf(condition);
 
-			return new Sequence(new If(condition),
-				new ParallelSequence("Parallel", new If(condition), trueAction));
+			return new INode[]
+			{
+				whileIf.InitialIfTrue,
+				new ParallelSequence("Parallel", whileIf.ParallelIfTrue, trueAction)
+			};
 		}
 
-		private static INode CreateFalseBranch(Delegates.Func<bool> condition, INode falseAction)
+		private static INode[] CreateIfElseNodes(Delegates.Func<bool> condition,
+			INode trueAction, INode falseAction)
 		{
 			if (condition == null)
 				throw new ArgumentNullException("condition");
+			if (trueAction == null)
+				throw new ArgumentNullException("trueAction");
 			if (falseAction == null)
 				throw new ArgumentNullException("falseAction");
 
-			return new Sequence(new Invert(new If(condition)),
-				new ParallelSequence("Parallel", new Invert(new If(condition)), falseAction));
+			var whileIf = new WhileIf(condition);
+
+			return new INode[]
+			{
+				new Sequence(whileIf.InitialIfTrue,
+					new ParallelSequence("Parallel", whileIf.ParallelIfTrue, trueAction)),
+				//new ParallelSequence("Parallel", new Invert(whileIf.ParallelIfFalse), falseAction)
+				new Sequence(whileIf.InitialIfFalse,
+					new ParallelSequence("Parallel", new Invert(whileIf.ParallelIfFalse), falseAction))
+			};
+		}
+
+		private class WhileIf
+		{
+			public readonly INode InitialIfTrue;
+			public readonly INode InitialIfFalse;
+			public readonly INode ParallelIfTrue;
+			public readonly INode ParallelIfFalse;
+			private bool lastVal;
+			private bool useLastInitialIfTrue;
+			private bool useLastInitialIfFalse;
+
+			public WhileIf(Delegates.Func<bool> condition)
+			{
+				this.InitialIfTrue = new If(() =>
+				{
+					this.lastVal = condition();
+					this.useLastInitialIfTrue = this.lastVal == true;
+
+					return this.lastVal;
+				});
+
+				this.InitialIfFalse = new If(() =>
+				{
+					this.useLastInitialIfFalse = this.lastVal == false;
+					return !this.lastVal;
+				});
+
+				this.ParallelIfTrue = new If(() =>
+				{
+					if (this.useLastInitialIfTrue)
+					{
+						this.useLastInitialIfTrue = false;
+						return true;
+					}
+
+					this.lastVal = condition();
+					return this.lastVal;
+				});
+
+				this.ParallelIfFalse = new If(() =>
+				{
+					if (this.useLastInitialIfFalse)
+					{
+						this.useLastInitialIfFalse = false;
+						return false;
+					}
+
+					this.lastVal = condition();
+					return this.lastVal;
+				});
+			}
 		}
 	}
 }
---
+++ UnitTests/Composites/WhileTests.cs
@@ -0,0 +1,153 @@
+﻿
+using NUnit.Framework;
+
+namespace BehaviorTree.Composites
+{
+	[TestFixture]
+	public class WhileTests
+	{
+		[Test]
+		// This test makes sure the While node only calls
+		// the condition once per behavior.Run() call.
+		public void WhileCountTests()
+		{
+			// This test makes sure
+			var ifValue = true;
+			var result = Result.Success;
+			var ifCount = 0;
+			var trueCount = 0;
+			var falseCount = 0;
+
+			var behavior = new Behavior(new While(() => { ifCount++; return ifValue; },
+				new Act(() => { trueCount++; return result; }),
+				new Act(() => { falseCount++; return result; })));
+
+			Asserts.Success(behavior);
+			Asserts.Counts(1, ifCount);
+			Asserts.Counts(1, trueCount);
+			Asserts.Counts(0, falseCount);
+
+			result = Result.Failure;
+			Asserts.Fail(behavior);
+			Asserts.Counts(2, ifCount);
+			Asserts.Counts(2, trueCount);
+			Asserts.Counts(0, falseCount);
+
+			ifValue = false;
+
+			result = Result.Success;
+			Asserts.Success(behavior);
+			Asserts.Counts(3, ifCount);
+			Asserts.Counts(2, trueCount);
+			Asserts.Counts(1, falseCount);
+
+			result = Result.Failure;
+			Asserts.Fail(behavior);
+			Asserts.Counts(4, ifCount);
+			Asserts.Counts(2, trueCount);
+			Asserts.Counts(2, falseCount);
+
+			ifValue = true;
+
+			Asserts.Fail(behavior);
+			Asserts.Counts(5, ifCount);
+			Asserts.Counts(3, trueCount);
+			Asserts.Counts(2, falseCount);
+
+			result = Result.Success;
+			Asserts.Success(behavior);
+			Asserts.Counts(6, ifCount);
+			Asserts.Counts(4, trueCount);
+			Asserts.Counts(2, falseCount);
+		}
+
+		[Test]
+		public void WhileExample1()
+		{
+			var attack1Count = 0;
+			var attack2Count = 0;
+
+			var attackTarget = new Sequence(
+				new Act("Attack1", () => { attack1Count++; return Result.Success; }),
+				new Act("Attack2", () => { attack2Count++; return Result.Running; }));
+
+			var canSeeTarget = false;
+
+			var behavior = new Behavior(new While(() => canSeeTarget, attackTarget));
+
+			Asserts.Fail(behavior);
+			Asserts.Counts(0, attack1Count, attack2Count);
+			Asserts.Fail(behavior);
+			Asserts.Counts(0, attack1Count, attack2Count);
+
+			canSeeTarget = true;
+
+			Asserts.Running(behavior,
+				"While/Parallel/If",
+				"While/Parallel/Sequence/Attack2");
+
+			Asserts.Counts(1, attack1Count, attack2Count);
+
+			Asserts.Running(behavior,
+				"While/Parallel/If",
+				"While/Parallel/Sequence/Attack2");
+
+			Asserts.Counts(1, attack1Count);
+			Asserts.Counts(2, attack2Count);
+
+			canSeeTarget = false;
+
+			Asserts.Fail(behavior);
+			Asserts.Counts(1, attack1Count);
+			Asserts.Counts(3, attack2Count);
+
+			Asserts.Fail(behavior);
+			Asserts.Counts(1, attack1Count);
+			Asserts.Counts(3, attack2Count);
+
+			canSeeTarget = true;
+
+			Asserts.Running(behavior,
+				"While/Parallel/If",
+				"While/Parallel/Sequence/Attack2");
+
+			Asserts.Counts(2, attack1Count);
+			Asserts.Counts(4, attack2Count);
+
+			Asserts.Running(behavior,
+				"While/Parallel/If",
+				"While/Parallel/Sequence/Attack2");
+
+			Asserts.Counts(2, attack1Count);
+			Asserts.Counts(5, attack2Count);
+		}
+
+		[Test]
+		public void WhileExample2()
+		{
+			var patrol = new Act("Patrol", () => Result.Running);
+			var attackTarget = new Act("Attack", () => Result.Running);
+
+			var canSeeTarget = false;
+			var behavior = new Behavior(new While(() => canSeeTarget, attackTarget, patrol));
+
+			Asserts.Running(behavior,
+				"While/Sequence/Parallel/!If",
+				"While/Sequence/Parallel/Patrol");
+
+			canSeeTarget = true;
+
+			Asserts.Fail(behavior);
+
+			Asserts.Running(behavior,
+				"While/Sequence/Parallel/If",
+				"While/Sequence/Parallel/Attack");
+
+			canSeeTarget = false;
+
+			Asserts.Running(behavior,
+				"While/Sequence/Parallel/!If",
+				"While/Sequence/Parallel/Patrol");
+		}
+	}
+}
